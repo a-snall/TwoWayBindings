c68580f443ede542997719bc9f1eb5f86064978d
Astrid Snäll <vaski2astrid@hotmail.com>
2023-02-19 16:57:37 -0500

    Initial Commit

---
+++ TwoWayBindings/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TwoWayBindings
+//
+//  Created by Astrid Snäll on 2/19/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
